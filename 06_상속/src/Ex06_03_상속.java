e62e3582aa25dd387eaf99280590f3cc27d1dd7b
Song <shg7248@gmail.com>
2022-05-26 08:55:35 +0900

    sdfdsf

--- 06_상속/src/Ex06_03_상속.java
+++ 06_상속/src/Ex06_03_상속.java
@@ -4,8 +4,11 @@ class Person2 {
 	String name;
 	int age;
 	
+	Person2() {
+		
+	}
+	
 	Person2(String name, int age) {
-		super();
 		this.name = name;
 		this.age = age;
 	}
@@ -21,6 +24,10 @@ class Employee2 extends Person2 {
 	String company;
 	String title;
 	
+	Employee2() {
+		
+	}
+	
 	Employee2(String name, int age, String company, String title) {
 		super(name, age);
 		this.company = company;
@@ -35,16 +42,34 @@ class Employee2 extends Person2 {
 	}
 } // Employee
 
+class Teacher extends Employee2 {
+	
+	String subject;
+	
+	public Teacher(String name, int age, String company, String title, String subject) {
+		super(name, age, company, title);
+		this.subject = subject;
+	}
+	
+	@Override
+	void show() {
+		super.show();
+		System.out.println("subject : " + subject);
+	}
+
+}
+
 public class Ex06_03_상속 {
 	public static void main(String[] args) {
 		
 		Person2 p = new Person2("강호동", 48);
 		Employee2 e = new Employee2("송은이", 21, "MBC", "과장");
+		Teacher t = new Teacher("아이유", 33, "서울고등학교", "부장교사", "음악");
 		
-		// show() 메서드 통해서 출력
 		p.show();
 		System.out.println();
 		e.show();	
-		
+		System.out.println();
+		t.show();	
 	}
 }
